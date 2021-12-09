37e50853d2de8736649b6a84e0aedeb693a943cb
Steven Ribeiro <steven24ribeiro@gmail.com>
2021-12-09 09:49:48 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Let's Go!!!</h1>
+    <h3>If you are ready, put ready next to your name ! </h3>
+    <span>Steven : Ready !</span><br>
+    <span>Akram : </span><br>
+    <span>Mike : </span><br>
+    <span>Michel : </span><br>
+
+
+</body>
+</html>
