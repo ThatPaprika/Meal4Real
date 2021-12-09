5ea12279bb75ceac28470d9f7504bae741769bd0
Steven Ribeiro <steven24ribeiro@gmail.com>
2021-12-09 15:50:54 +0100

    homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,23 +9,27 @@
     <title>Document</title>
 </head>
 
-<body>
-    <div class="container-fluid">
-        <div class="header d-flex justify-content-center align-items-center" style="height:80px;">
-            
-                <img src="Logo.jpg" alt="" style="height:60px">
-            
+<body style="background-color:#E3E3E3">
+    <div class="container-fluid p-3">
+        <div style="height:10%"></div>
+        <div class="d-flex flex-column align-items-center justify-content-center" style="height:30%">
+            <h2>We are Meal4Real</h2>
+            <div class="mt-5 mb-5 logo" style="height:120px;width:120px;border-radius:140px;background-color:#C4C4C4">
+            </div>
         </div>
-        <h1>Let's Go!!!</h1>
-        <h3>If you are ready, put ready next to your name ! </h3>
-        <span>Steven : Ready !</span><br>
-        <span>Akram : Ready </span><br>
-        <span>Michel : Ready </span><br>
-        <span>Mike : Ready !</span><br>
 
-        <div class="footer"></div>
+
+        <div class="d-flex flex-column justify-content-end" style="height:50%" >
+            <button class="mt-3"
+                style="width:100%;height:54px;background-color:rgba(129, 178, 20, 0.9); border:0; border-radius:30px;font-weight:bold">
+                Sign In</button>
+            <button class="mt-3"
+                style="width:100%;height:54px;background-color:rgba(129, 178, 20, 0.9); border:0; border-radius:30px;font-weight:bold">
+                Sign Up</button>
+
+        </div>
     </div>
-    
+
 
 
 
