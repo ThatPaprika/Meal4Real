72dcba290047c97dacf4766954d8f01c75c9c91a
Fernandes <mikfernandesinf@gmail.com>
2021-12-13 09:54:47 +0100

    Routes
    routes changed

--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,39 @@ Route::get('/home', function () {
 Route::get('/add_food', function () {
     return view('add_food');
 });
+
+Route::get('/thankyou', function () {
+    return view('thank_you');
+});
+
+Route::get('/food_list', function () {
+    return view('food_list');
+});
+
+Route::get('/forgot_password', function () {
+    return view('forgot_password');
+});
+
+Route::get('/reset_password', function () {
+    return view('reset_password');
+});
+
+Route::get('/profile', function () {
+    return view('profile');
+});
+
+Route::get('/admin', function () {
+    return view('admin');
+});
+
+Route::get('/about_us', function () {
+    return view('about_us');
+});
+
+Route::get('/details', function () {
+    return view('details');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
