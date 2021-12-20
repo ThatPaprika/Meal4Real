b507114ab151ab44a1eedc2543f0ed3580212103
Steven Ribeiro <steven24ribeiro@gmail.com>
2021-12-20 09:38:45 +0100

    fixes profile

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -127,11 +127,7 @@
                             placeholder="Password" aria-label="Username" aria-describedby="basic-addon1"
                             :value="old('email')" required autocomplete="current-password">
                     </div>
-                    <!-- Captcha Form ! (put to false if u wanna hidde it) -->
-                    <div class="mb-3 mt-5">
-                        {!! NoCaptcha::renderJs('en', false, 'onloadCallback') !!}
-                        {!! NoCaptcha::display() !!}
-                    </div>
+
                    
 
 
