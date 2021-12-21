eef48ef0e0045c96efd117998004f9f34f64b1f2
Akram <ademak465@gmail.com>
2021-12-21 18:49:03 +0100

    register and login page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -88,11 +88,8 @@
 
     </div>
     <div class="d-none d-sm-block container-fluid p-0" style="height:100vh;position:relative;background-color:white">
-        <div class="p-5" style="height:40%;background-color:lightgray">
-            <div style="width:200px">
-                <h2>Sign in</h2>
-                <p>Bla dmlk lkhj mlkj mlkj mlkj mlkj mlkj dsf ds dsf sdf dsf sdf sdf sdf sdf sdf sdf </p>
-            </div>
+        <div class="p-5" style="height:100%;background-image: linear-gradient(#ceb006, green)">
+
         </div>
 
         <div
